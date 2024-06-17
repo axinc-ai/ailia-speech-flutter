445d359bc461566d2cb290199db64f9a58cbf7ec
Kazuki Kyakuno <kyakuno@axell.co.jp>
2024-06-17 22:13:51 +0900

    Fix iOS link error

---
+++ ios/Classes/AiliaSpeechPluginPreventStrip.c
@@ -0,0 +1,13 @@
+//
+//  AiliSpeechPluginPreventStrip.c
+//
+//  Created by Kazuki Kyakuno on 2023/07/31.
+//
+
+// Dummy link to keep libailia_tokenizer.a from being deleted
+
+extern const char* AILIA_API ailiaSpeechGetErrorDetail(void* net);
+
+void test(void){
+    ailiaSpeechGetErrorDetail(0);
+}
